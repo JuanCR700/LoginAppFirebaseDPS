505882b8cb1a8cb3c4bf9ebfb4c63b49a4b0482a
JC <ssjramirez69@gmail.com>
2025-05-04 21:36:48 -0600

    codigo trabajado

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWpuXzHNJXXIWOLtHjY4_t-08piuOtjb4',
+  authDomain: 'pruebadps.firebaseapp.com',
+  projectId: 'pruebadps-97a4d',
+  storageBucket: 'pruebadps.appspot.com',
+  messagingSenderId: '400165564984',
+  appId: 'TU_APP_ID'
+};
+
+export const app = initializeApp(firebaseConfig);
